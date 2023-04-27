59f6099846922ccc7717664d7900b4120ddc9fd6
ILYAS KERBAL <kerbalsc@gmail.com>
2023-04-27 17:51:55 +0100

    Settings viewModel: uiState with combined state flows

---
+++ settingsapp/src/main/java/io/github/ilyaskerbal/settingsapp/SettingsViewModel.kt
@@ -0,0 +1,28 @@
+package io.github.ilyaskerbal.settingsapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.*
+
+class SettingsViewModel: ViewModel() {
+
+	private val _notificationEnabled: MutableStateFlow<Boolean> = MutableStateFlow(true)
+	private val _hintsEnabled: MutableStateFlow<Boolean> = MutableStateFlow(true)
+	private val _marketingOption: MutableStateFlow<MarketingOption> = MutableStateFlow(MarketingOption.ACCEPT)
+	private val _themeOption: MutableStateFlow<ThemeOption> = MutableStateFlow(ThemeOption.SYSTEM)
+
+	val uiState: StateFlow<SettingsState> = combine(
+		_notificationEnabled, _hintsEnabled, _marketingOption, _themeOption
+	) { notificationEnabled, hintsEnabled, marketingOption, themeOption ->
+		SettingsState(
+			notificationEnabled = notificationEnabled,
+			showHintsEnabled = hintsEnabled,
+			marketingOption = marketingOption,
+			themeOption = themeOption
+		)
+	}.stateIn(
+		scope = viewModelScope,
+		started = SharingStarted.WhileSubscribed(),
+		initialValue = SettingsState()
+	)
+}
