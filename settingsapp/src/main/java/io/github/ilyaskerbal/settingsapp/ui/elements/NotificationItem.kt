4183d408d611415973ce387442855ec79a506d44
ILYAS KERBAL <kerbalsc@gmail.com>
2023-04-30 19:22:50 +0100

    Accessibility: Notification setting item

--- settingsapp/src/main/java/io/github/ilyaskerbal/settingsapp/ui/elements/NotificationItem.kt
+++ settingsapp/src/main/java/io/github/ilyaskerbal/settingsapp/ui/elements/NotificationItem.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +12,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.ilyaskerbal.settingsapp.R
 
@@ -21,17 +25,30 @@ fun NotificationIem(
 	onToggle: (Boolean) -> Unit = {},
 	state: Boolean
 ) {
+	val stateDescription = if (state)
+		stringResource(id = R.string.cd_notifications_enabled)
+	else stringResource(id = R.string.cd_notifications_disabled)
+
 	SettingsItem(modifier = modifier) {
 		Row(
-			modifier = Modifier.fillMaxWidth()
-				.padding(horizontal = dimensionResource(id = R.dimen.item_padding)),
+			modifier = Modifier
+				.toggleable(
+					value = state,
+					role = Role.Switch,
+					onValueChange = onToggle
+				)
+				.semantics {
+					this.stateDescription = stateDescription
+				}
+				.fillMaxWidth()
+				.padding(dimensionResource(id = R.dimen.item_padding)),
 			verticalAlignment = Alignment.CenterVertically
 		) {
 			Text(
 				modifier = Modifier.weight(1f),
 				text = stringResource(id = title)
 			)
-			Switch(checked = state, onCheckedChange = onToggle)
+			Switch(checked = state, onCheckedChange = null)
 		}
 	}
 }
