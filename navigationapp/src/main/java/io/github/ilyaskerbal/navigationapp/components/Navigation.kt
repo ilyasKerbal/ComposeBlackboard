0bd765fb1f1962edd439bbe2968fc32af31d888a
ILYAS KERBAL <kerbalsc@gmail.com>
2023-06-15 00:41:42 +0100

    Nested naviagtion setup

--- navigationapp/src/main/java/io/github/ilyaskerbal/navigationapp/components/Navigation.kt
+++ navigationapp/src/main/java/io/github/ilyaskerbal/navigationapp/components/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import io.github.ilyaskerbal.navigationapp.utils.Destination
 
 @Composable
@@ -16,31 +17,36 @@ fun Navigation(
 	NavHost(
 		modifier = modifier,
 		navController = navHostController,
-		startDestination = Destination.Feed.path
+		startDestination = Destination.Home.path
 	) {
-		composable(
-			route = Destination.Feed.path
+		navigation(
+			startDestination = Destination.Feed.path,
+			route = Destination.Home.path
 		) {
-			ContentArea(
-				destination = Destination.Feed,
-				modifier = Modifier.fillMaxSize()
-			)
-		}
-		composable(
-			route = Destination.Contacts.path
-		) {
-			ContentArea(
-				destination = Destination.Contacts,
-				modifier = Modifier.fillMaxSize()
-			)
-		}
-		composable(
-			route = Destination.Calendar.path
-		) {
-			ContentArea(
-				destination = Destination.Calendar,
-				modifier = Modifier.fillMaxSize()
-			)
+			composable(
+				route = Destination.Feed.path
+			) {
+				ContentArea(
+					destination = Destination.Feed,
+					modifier = Modifier.fillMaxSize()
+				)
+			}
+			composable(
+				route = Destination.Contacts.path
+			) {
+				ContentArea(
+					destination = Destination.Contacts,
+					modifier = Modifier.fillMaxSize()
+				)
+			}
+			composable(
+				route = Destination.Calendar.path
+			) {
+				ContentArea(
+					destination = Destination.Calendar,
+					modifier = Modifier.fillMaxSize()
+				)
+			}
 		}
 	}
 }
